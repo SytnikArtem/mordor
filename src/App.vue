046dd965601f183073efa96b64a07342d57ca26e
Oleksandr Sukhinin <syhinin@gmail.com>
2022-02-28 08:09:33 +0100

    add internationalozation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <LanguageSwitcher />
     <HelloWorld />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import LanguageSwitcher from "./components/LanguageSwitcher.vue";
 
 export default {
   name: "App",
   components: {
+    LanguageSwitcher,
     HelloWorld,
   },
 };
